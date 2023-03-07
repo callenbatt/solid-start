90db3069a9e6e579b8cf48543328174f476d45a0
callenbatt@gmail.com <callenbatt@gmail.com>
2023-03-06 21:47:33 -0500

    edited meta tag

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,10 @@
-import { Title } from "solid-start";
+import { Meta, Title } from "solid-start";
 
 export default function Home() {
   return (
     <main>
       <Title>About | Chris Batt</Title>
+      <Meta name="description" content="Information about me"></Meta>
       <h1>Hey, this is all about me</h1>
     </main>
   );
