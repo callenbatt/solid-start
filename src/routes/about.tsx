dc6198942904dfb900a845d33662dc5f2866093d
callenbatt@gmail.com <callenbatt@gmail.com>
2023-03-06 21:33:17 -0500

    testing routing

---
+++ src/routes/about.tsx
@@ -0,0 +1,10 @@
+import { Title } from "solid-start";
+
+export default function Home() {
+  return (
+    <main>
+      <Title>About | Chris Batt</Title>
+      <h1>Hey, this is all about me</h1>
+    </main>
+  );
+}
